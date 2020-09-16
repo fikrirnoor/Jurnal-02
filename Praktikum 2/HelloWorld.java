9829eea809c9e39555097a177bbf60235a2e86d9
fikrirnoor <fikrirahmanda31@icloud.com>
2020-09-16 14:17:52 +0700

    HelloWorld.java

---
+++ Praktikum 2/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Nama: Fikri Rahmanda Noor");
+		System.out.println("Nim: 6706200045");
+		System.out.println("Kelas: D3IF4404");
+	}
+}
